99965fa7b849f7ba8bba12e5325b40c7d05bd21c
Eliel Ferreira <eliel_elie@hotmail.com>
2025-08-26 20:08:24 -0300

    Refactor activemq configuration for concise and improved readability

--- config/activemq.php
+++ config/activemq.php
@@ -4,40 +4,40 @@ use Stomp\Protocol\Version;
 
 return [
 
-    'driver'        => 'activemq',
-    'read_queues'   => env('ACTIVEMQ_READ_QUEUES'),
-    'write_queues'  => env('ACTIVEMQ_WRITE_QUEUES'),
-    'protocol'      => env('ACTIVEMQ_PROTOCOL', 'tcp'),
-    'host'          => env('ACTIVEMQ_HOST', '127.0.0.1'),
-    'port'          => env('ACTIVEMQ_PORT', 61613),
-    'username'      => env('ACTIVEMQ_USERNAME', 'admin'),
-    'password'      => env('ACTIVEMQ_PASSWORD', 'admin'),
+    'driver'       => 'activemq',
+    'read_queues'  => env('ACTIVEMQ_QUEUE'),
+    'write_queues' => env('ACTIVEMQ_QUEUE'),
+    'protocol'     => env('ACTIVEMQ_PROTOCOL', 'tcp'),
+    'host'         => env('ACTIVEMQ_HOST', '127.0.0.1'),
+    'port'         => env('ACTIVEMQ_PORT', 61613),
+    'username'     => env('ACTIVEMQ_USERNAME', 'admin'),
+    'password'     => env('ACTIVEMQ_PASSWORD', 'admin'),
 
-    /**
+    /*
      * Set to "horizon" if you wish to use Laravel Horizon.
      */
-    'worker'        => env('ACTIVEMQ_WORKER', 'default'),
+    'worker' => env('ACTIVEMQ_WORKER', 'default'),
 
-    /**
+    /*
      * Calculate tries and backoff automatically without the need to specify it
      * in the queue work command.
      */
-    'auto_tries'    => env('ACTIVEMQ_AUTO_TRIES', true),
-    'auto_backoff'  => env('ACTIVEMQ_AUTO_BACKOFF', true),
+    'auto_tries'   => env('ACTIVEMQ_AUTO_TRIES', true),
+    'auto_backoff' => env('ACTIVEMQ_AUTO_BACKOFF', true),
 
-    /**
+    /*
      * If all messages should fail on timeout.
      * Set to false in order to revert to default (looking in event payload)
      */
     'fail_on_timeout' => env('ACTIVEMQ_FAIL_ON_TIMEOUT', true),
 
-    /**
+    /*
      * Maximum time in seconds for job execution.
      * This value must be less than send heartbeat in order to run correctly.
      */
-    'timeout'       => env('ACTIVEMQ_TIMEOUT', 10),
+    'timeout' => env('ACTIVEMQ_TIMEOUT', 10),
 
-    /**
+    /*
      * Incremental multiplier for failed job redelivery.
      * Multiplier 2 means that the rescheduled job will be repeated in attempt^2 seconds.
      *
@@ -49,44 +49,53 @@ return [
      */
     'backoff_multiplier' => env('ACTIVEMQ_BACKOFF_MULTIPLIER', 2),
 
-    /**
+    /*
      * What will be appended as the queue if only address/topic is defined.
      * This is done to ensure that service doesn't connect to a broker with
      * hash as queue name. In case of multiple services connecting in such
      * a way, it becomes unclear which queue is from which service.
      */
-    'default_queue'     => env('ACTIVEMQ_DEFAULT_QUEUE','default'),
+    'default_queue' => env('ACTIVEMQ_DEFAULT_QUEUE', 'default'),
 
-    'read_queues'     => env('ACTIVEMQ_QUEUE'),
-
-    'write_queues'     => env('ACTIVEMQ_QUEUE'),
-
-    /**
+    /*
      * Use Laravel logger for outputting logs.
      */
-    'enable_logs'       => env('ACTIVEMQ_LOGS', false) === true,
+    'enable_logs' => env('ACTIVEMQ_LOGS', false) === true,
 
-    /**
+    /*
      * Should the read queues be prepended. Useful for i.e. Artemis where queue
      * name is unique across whole broker instance. This will thus add some
      * uniqueness to the queues.
      */
-    'prepend_queues'    => true,
+    'prepend_queues' => true,
 
     'persistent_queues' => env('ACTIVEMQ_PERSISTENT', false) === true,
 
-    /**
+    /*
      * Heartbeat which will be requested from server at given millisecond period.
      */
     'receive_heartbeat' => env('ACTIVEMQ_RECEIVE_HEARTBEAT', 0),
 
-    /**
+    /*
      * Heartbeat which we will be sending to server at given millisecond period.
      */
-    'send_heartbeat'    => env('ACTIVEMQ_SEND_HEARTBEAT', 20000),
+    'send_heartbeat' => env('ACTIVEMQ_SEND_HEARTBEAT', 20000),
 
-    /**
+    /*
      * Array of supported versions.
      */
-    'version'           => [Version::VERSION_1_2],
+    'version' => [Version::VERSION_1_2],
+
+    /*
+     * Horizon compatibility settings
+     */
+    'horizon' => [
+        'enabled' => env('ACTIVEMQ_HORIZON_ENABLED', false),
+
+        /*
+         * When using Horizon, this determines if workers should be isolated
+         * per queue or if they can process multiple queues.
+         */
+        'isolate_workers' => env('ACTIVEMQ_HORIZON_ISOLATE', true),
+    ],
 ];
